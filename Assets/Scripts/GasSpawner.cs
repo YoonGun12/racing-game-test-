87b6539668c316178555a909c25049ed53bae25f
Yoon Gun <dbsrjs0514@naver.com>
2025-01-22 14:37:27 +0900

    가스 스폰 구현

---
+++ Assets/Scripts/GasSpawner.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GasSpawner : MonoBehaviour
+{
+    public BoxCollider gasSpawnPoint;
+    public GameObject gasPrefab;
+    private float timer = 0;
+    private float gasSpawnTime = 3f;
+
+
+    private void Update()
+    { 
+        timer += Time.deltaTime;
+        if (timer > gasSpawnTime)
+        {
+            SpawnGas();
+        }
+    }
+
+    void SpawnGas()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(gasSpawnPoint.bounds.min.x, gasSpawnPoint.bounds.max.x),
+            gasSpawnPoint.transform.position.y, gasSpawnPoint.transform.position.z);
+        GameObject spawnedGas = Instantiate(gasPrefab, spawnPos, Quaternion.identity);
+        timer = 0;
+    }
+}

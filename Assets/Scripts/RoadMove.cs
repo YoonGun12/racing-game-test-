27c3ac8d09b4a593b2f4e7c1f74b65aee877a0d3
Yoon Gun <dbsrjs0514@naver.com>
2025-01-22 14:23:18 +0900

    도로 이동 구현

---
+++ Assets/Scripts/RoadMove.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMove : MonoBehaviour
+{
+    public float objMoveSpeed;
+
+
+    private void Update()
+    {
+        transform.position -= Vector3.forward * (objMoveSpeed * Time.deltaTime);
+
+        if (transform.position.z <= -21.5f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 21.5f);
+        }
+    }
+
+    
+}

ffb45a385e4f8b2551fe18714882fb094e488dc7
Yoon Gun <dbsrjs0514@naver.com>
2025-01-22 14:02:52 +0900

    맵 구조 및 플레이어 이동 구현

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 moveVec = new Vector3(h, 0, v);
+        rigid.velocity = moveVec * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/SpawnRoad.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoad : MonoBehaviour
+{
+    public GameObject roadPrefab;
+    public Transform roadSpawnPoint;
+    
+    
+
+
+}
